60915be015957938350ee137745bdffd783d91a1
Chris Henn <chris@chrishenn.net>
2015-02-20 12:11:46 -0800

    Add and import Skeleton CSS dependency

--- Brocfile.js
+++ Brocfile.js
@@ -5,5 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/d3/d3.js');
+app.import('bower_components/skeleton-css/css/normalize.css');
+app.import('bower_components/skeleton-css/css/skeleton.css');
 
 module.exports = app.toTree();
